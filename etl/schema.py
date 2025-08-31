94a0d5f9d9aaba119c6c3f1f48b0032d9ffa8e79
Nin-ja-12 <160606439+Nin-ja-12@users.noreply.github.com>
2025-08-31 14:01:06 +0200

    Update schema.py

--- etl/schema.py
+++ etl/schema.py
@@ -1,11 +1,11 @@
 from pyspark.sql.types import (
     StructType, StructField, StringType, IntegerType,
-    DoubleType, TimestampType, DateType
+    DoubleType, TimestampType, DateType , LongType, DecimalType
 )
 
 contract_schema = StructType([
     StructField("SOURCE_SYSTEM", StringType()),
-    StructField("CONTRACT_ID", IntegerType()),
+    StructField("CONTRACT_ID", LongType()), 
     StructField("CONTRACT_TYPE", StringType()),
     StructField("INSURED_PERIOD_FROM", StringType()),
     StructField("INSURED_PERIOD_TO", StringType()),
@@ -16,9 +16,9 @@ claim_schema = StructType([
     StructField("SOURCE_SYSTEM", StringType()),
     StructField("CLAIM_ID", StringType()),
     StructField("CONTRACT_SOURCE_SYSTEM", StringType()),
-    StructField("CONTRACT_ID", IntegerType()),
+    StructField("CONTRACT_ID", LongType()),
     StructField("CLAIM_TYPE", StringType()),
     StructField("DATE_OF_LOSS", DateType()),
-    StructField("AMOUNT", DoubleType()),
+    StructField("AMOUNT", DecimalType(16, 5)), 
     StructField("CREATION_DATE", TimestampType())
 ])
